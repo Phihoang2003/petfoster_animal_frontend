04ca5eae76bb8c45ba22a5ec84cfba7e6c0a1c83
Đặng Hoàng Phi <107670709+Phihoang2003@users.noreply.github.com>
2024-09-02 18:53:35 +0700

    done custom main button

---
+++ src/components/buttons/MainButton.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { MouseEventHandler } from "react";
+import classNames from "classnames";
+import WrapperAnimation from "@/components/animations/WrapperAnimation";
+import CustomButton from "@/components/buttons/CustomButton";
+
+export interface IMainButtonProps {
+  className?: string;
+  title: string;
+  width?: number | string;
+  height?: number | string;
+  background?: string;
+  href?: string;
+  upercase?: boolean;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+}
+
+export default function MainButton({
+  className,
+  title,
+  width,
+  height,
+  background = "bg-[#5FA503]",
+  href,
+  upercase = true,
+  onClick,
+}: IMainButtonProps) {
+  return (
+    <WrapperAnimation onClick={onClick} hover={{ y: -4 }}>
+      <CustomButton
+        href={href}
+        style={width || height ? { width: width, height: height } : undefined}
+        className={classNames(
+          " text-1xl font-medium text-white py-[14px] px-8 rounded-md flex items-center justify-center",
+          {
+            [className ?? ""]: Boolean(className),
+            "w-[188px]": !Boolean(width),
+            "h-[48px]": !Boolean(height),
+            [background]: true,
+          }
+        )}
+      >
+        <span
+          className={classNames("", {
+            ["uppercase"]: upercase,
+          })}
+        >
+          {title}
+        </span>
+      </CustomButton>
+    </WrapperAnimation>
+  );
+}
