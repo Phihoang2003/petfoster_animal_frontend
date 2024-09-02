ee14d81915200125da6c33d3aa714f11ef0a7e13
Đặng Hoàng Phi <107670709+Phihoang2003@users.noreply.github.com>
2024-09-02 22:37:20 +0700

    add interface impace,pet and post

---
+++ src/configs/interface.d.ts
@@ -0,0 +1,31 @@
+export interface IImpact {
+  image: string;
+  quantity: string;
+  title: string;
+  prefix?: string;
+}
+export interface IPet {
+  id: string | number;
+  name: string;
+  image: string;
+  breed: string;
+  type: string;
+  size: string;
+  sex: string;
+  fostered: string | number;
+  description: string;
+  fosterDate: number;
+  like: boolean;
+  adoptAt?: string | null;
+}
+export interface IPost {
+  id: number | string;
+  title: string;
+  thumbnail: string;
+  // contents: string;
+  containVideo?: boolean;
+  isLike: boolean;
+  likes: number;
+  comments: number;
+  user: IProfile;
+}
