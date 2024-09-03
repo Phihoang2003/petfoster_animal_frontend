d81ed03ec024fcfa2d0a23583f02946f2805efe4
Đặng Hoàng Phi <107670709+Phihoang2003@users.noreply.github.com>
2024-09-03 11:05:42 +0700

    complete div animaiton when scroll

---
+++ src/components/animations/DivAnimation.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { motion, useAnimation, useInView } from "framer-motion";
+import React, { ReactNode, useEffect, useRef } from "react";
+
+export interface IDivAnitmationProps {
+  children: ReactNode;
+  className?: string;
+  delay?: number;
+  duration?: number;
+  variants?: {};
+}
+export default function DivAnimation({
+  children,
+  className,
+  delay = 0.1,
+  duration = 0.5,
+  variants = {
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0 },
+  },
+}: IDivAnitmationProps) {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const mainControl = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControl.start("visible");
+    } else {
+      mainControl.start("hidden");
+    }
+  }, [isInView, mainControl]);
+  return (
+    <motion.div
+      ref={ref}
+      variants={variants}
+      initial={"hidden"}
+      animate={mainControl}
+      transition={{ duration, delay }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+}
