910c75c2b618f2b63b9899aec845cf89971270ae
Đặng Hoàng Phi <107670709+Phihoang2003@users.noreply.github.com>
2024-09-04 17:40:28 +0700

    add data footer

---
+++ src/data/footer.ts
@@ -0,0 +1,38 @@
+import {
+  faEnvelope,
+  faHouse,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const dataFooter = {
+  petfoster: `Welcome to our animal rescue website and pet community! We not only provide pet products but also a platform where you can share precious moments of your furry friends on social media. Join us in creating a loving and supportive community for all animals!`,
+  aboutUs: [
+    {
+      title: "Terms & privacy policy",
+      link: "/",
+    },
+    {
+      title: "Warranty & return policy",
+      link: "/",
+    },
+    {
+      title: "Frequently asked questions",
+      link: "/",
+    },
+  ],
+  contacts: [
+    {
+      title: "288 Dang Thuc Vinh, Thoi Tam Thon, Hoc Mon, Ho Chi Minh",
+      icon: faHouse,
+    },
+    {
+      title: "info@petfoser.com",
+      icon: faEnvelope,
+    },
+    {
+      title: "0913842870",
+      icon: faPhone,
+    },
+  ],
+  coppyRight: `© 2024 PetFoster . All Rights Reserved.`,
+};
