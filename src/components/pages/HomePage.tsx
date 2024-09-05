a627f7511b09d649ef68f44bbbba93a79bf94656
Đặng Hoàng Phi <107670709+Phihoang2003@users.noreply.github.com>
2024-09-05 14:38:06 +0700

    done get pets for home page

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import { homePageData } from "@/data/home-page";
 import ImpactOfYear from "@/components/impacts/ImpactOfYear";
 import AboutCom from "@/components/common/common-components/AboutCom";
+import Pets from "@/components/products-and-pets/Pets";
+import { links } from "@/data/links";
+import { dataTakeAction } from "@/data/adopt";
 export default function HomePage() {
   return (
     <>
       <ImpactOfYear data={(homePageData && homePageData.impactOfYear) || []} />
       <AboutCom />
+      <Pets
+        options={{
+          buttonTitle: "See all",
+          href: links.pets.adoptPage,
+        }}
+        data={(dataTakeAction && dataTakeAction.pets) || []}
+      />
     </>
   );
 }
