d68c698d68ca0734984e87321459acbbe9421763
Đặng Hoàng Phi <107670709+Phihoang2003@users.noreply.github.com>
2024-09-05 14:36:03 +0700

    config nextjs for  link

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "t0.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ichef.bbci.co.uk",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
