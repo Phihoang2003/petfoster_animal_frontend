94e4d4e635b84b39388c6e12c71b8b42b851271a
Đặng Hoàng Phi <107670709+Phihoang2003@users.noreply.github.com>
2024-09-05 14:37:51 +0700

    add format capitalize

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -2,3 +2,8 @@ export const stringToUrl = (string: string) => {
   if (!string || string.length <= 0) return "";
   return string.toLowerCase().replaceAll(" ", "-");
 };
+export function capitalize(value: string) {
+  if (value.length < 1) return value;
+
+  return value;
+}
