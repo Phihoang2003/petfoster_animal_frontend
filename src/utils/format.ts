5ca8632ccf9ffdf7bef06e93f23ffcc62c38a074
Đặng Hoàng Phi <107670709+Phihoang2003@users.noreply.github.com>
2024-09-06 21:55:11 +0700

    format toGam and toCurrency for price

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -7,3 +7,14 @@ export function capitalize(value: string) {
 
   return value;
 }
+export const toGam = (value: number) => {
+  return value < 1000 ? value + "g" : (value / 1000).toFixed(1) + "kg";
+};
+export const toCurrency = (price: number): string => {
+  return new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+  })
+    .format(price)
+    .replace("₫", "VND");
+};
