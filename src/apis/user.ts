df263ed53adb91c63d85fdd175ea698c6c0f0dca
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-09-08 14:41:01 +0700

    add refresh verify code

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,10 +1,42 @@
-import { ApiRegister } from "@/configs/types";
+import {
+  ApiRefreshVerifyCode,
+  ApiRegister,
+  ApiVerifyCode,
+} from "@/configs/types";
 import axios from "../configs/axios";
 export const register: ApiRegister = async (data) => {
   const res = await axios({
     method: "POST",
     url: "register",
     data,
+  });
+
+  if (!res) return null;
+
+  return res?.data;
+};
+
+export const refreshVerifyCode: ApiRefreshVerifyCode = async (code: string) => {
+  const res = await axios({
+    method: "GET",
+    url: "refresh-code",
+    params: {
+      code,
+    },
+  });
+
+  if (!res) return null;
+
+  return res?.data;
+};
+
+export const verifyCode: ApiVerifyCode = async (code: string) => {
+  const res = await axios({
+    method: "GET",
+    url: "/verify",
+    params: {
+      code,
+    },
   });
 
   if (!res) return null;
