87995d5c7cc3c6523577ba3d7f03c2f9eaf48d6f
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-09-09 11:24:30 +0700

    add role type and api get current user

--- src/configs/types.ts
+++ src/configs/types.ts
@@ -1,11 +1,13 @@
 import {
   IBaseResponse,
   IPet,
+  IProfile,
   IRequestFilterPet,
   ISignDataResponse,
   PagiantionResponse,
 } from "@/configs/interface";
-
+import { store } from "@/redux/store";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 export type ValidTags = keyof JSX.IntrinsicElements;
 
 export type LocationTileType = "center" | "left" | "right";
@@ -17,6 +19,8 @@ export type StateType =
   | "cancelled"
   | "cancelled_by_admin"
   | "cancelled_by_customer";
+
+export type RoleType = "ROLE_USER" | "ROLE_STAFF" | "ROLE_ADMIN" | "ROLE_SUPER";
 export type ValidateType = { message: string; error: boolean };
 
 export type ApiFilterPets = (
@@ -30,6 +34,7 @@ export type ApiRegister = (
 export type ApiRefreshVerifyCode = (
   code: string
 ) => Promise<IBaseResponse<any>>;
+export type ApiGetCurUser = () => Promise<IBaseResponse<IProfile>>;
 
 export type ApiVerifyCode = (code: string) => Promise<IBaseResponse<any>>;
 
@@ -41,3 +46,12 @@ export type RegisterFormData = {
   password: string;
   confirmPassword: string;
 };
+export type MenuHeaderType = {
+  title: string;
+  href: string;
+  icon: IconProp;
+};
+export type UserFormType = { username: string; password: string };
+export type ApiLogin = (data: UserFormType) => Promise<ISignDataResponse>;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
