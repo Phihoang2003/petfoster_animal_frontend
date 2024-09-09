0f79db78ff699b608e8482322643a32ed71b6ef9
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-09-09 11:24:56 +0700

    custom menu user for load user when login success

---
+++ src/components/common/common-headers/MenuUser.tsx
@@ -0,0 +1,91 @@
+"use client";
+import WrapperAnimation from "@/components/animations/WrapperAnimation";
+import { MenuHeaderType, RootState } from "@/configs/types";
+import { listProfile } from "@/data/header";
+import { links } from "@/data/links";
+import { useAppSelector } from "@/hooks/reduxHooks";
+import { contants } from "@/utils/constant";
+import { faChartLine } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Avatar } from "@mui/material";
+import Tippy from "@tippyjs/react/headless";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+export interface IMenuUserProps {
+  isChangeBg?: boolean;
+}
+export default function MenuUser({ isChangeBg }: IMenuUserProps) {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const [isClient, setisClient] = useState(false);
+  const { user } = useAppSelector((state: RootState) => state.userReducer);
+
+  let listProfileBeforeCheck: MenuHeaderType[];
+  if (user && contants.roles.manageRoles.includes(user?.role)) {
+    listProfileBeforeCheck = [
+      {
+        title: "Dashboard",
+        href: links.admin,
+        icon: faChartLine,
+      },
+      ...listProfile,
+    ];
+  } else {
+    listProfileBeforeCheck = [...listProfile];
+  }
+  useEffect(() => {
+    setisClient(true);
+  }, []);
+  return (
+    <div className="flex gap-4">
+      {isClient ? (
+        <Tippy
+          interactive
+          visible={openMenu}
+          onClickOutside={() => setOpenMenu(false)}
+          placement="bottom-end"
+          render={(attr) => {
+            return (
+              <ul
+                className="w-[188px] bg-[#F2F2F2] text-[#4C4C4C] rounded-lg overflow-hidden shadow-xl"
+                tabIndex={0}
+                {...attr}
+              >
+                {listProfileBeforeCheck.map((profile, index) => {
+                  return (
+                    <li
+                      key={profile.href}
+                      className="cursor-pointer w-full hover:bg-green-65a30d transition-all ease-linear duration-1500 hover:text-white px-3 h-10 flex items-center pl-6"
+                    >
+                      <Link
+                        className="w-full flex items-center gap-[10px]"
+                        href={profile.href}
+                      >
+                        <FontAwesomeIcon
+                          className="w-4 h-4"
+                          icon={profile.icon}
+                        />
+                        <span>{profile.title}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            );
+          }}
+        >
+          <div className="flex items-center justify-between gap-4 cursor-pointer select-none">
+            <WrapperAnimation onClick={() => setOpenMenu(true)} hover={{}}>
+              <Avatar
+                alt="avartar"
+                className="cursor-pointer border-2"
+                src={user?.avatar || contants.avartarDefault}
+              />
+            </WrapperAnimation>
+          </div>
+        </Tippy>
+      ) : null}
+    </div>
+  );
+}
