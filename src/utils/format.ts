e5afe028850dfa8c3ad780b109877ab47187f825
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-09-13 20:46:40 +0700

    add format to abbrevNumber

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -18,3 +18,19 @@ export const toCurrency = (price: number): string => {
     .format(price)
     .replace("₫", "VND");
 };
+const floor = Math.floor,
+  abs = Math.abs,
+  log = Math.log,
+  round = Math.round,
+  min = Math.min;
+const abbrev = ["K", "Mil", "Bil"];
+function rnd(n: number, precision: number) {
+  const prec = 10 ** precision;
+  return round(n * prec) / prec;
+}
+export function toAbbrevNumber(n: number) {
+  let base = floor(log(abs(n)) / log(1000));
+  const suffix = abbrev[min(abbrev.length - 1, base - 1)];
+  base = abbrev.indexOf(suffix) + 1;
+  return suffix ? rnd(n / 1000 ** base, 2) + suffix : "" + n;
+}
