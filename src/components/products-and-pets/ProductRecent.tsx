4a9a4f8051e22a555a43599b6bb2735af83992bf
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-06 15:41:44 +0700

    done product recent for suggestion

--- src/components/products-and-pets/ProductRecent.tsx
+++ src/components/products-and-pets/ProductRecent.tsx
@@ -1,3 +1,4 @@
+import WrapperAnimation from "@/components/animations/WrapperAnimation";
 import { IProduct } from "@/configs/interface";
 import { links } from "@/data/links";
 import { stringToUrl, toCurrency } from "@/utils/format";
@@ -12,40 +13,42 @@ export interface IProductRecentProps {
 export default function ProductRecent({ data }: IProductRecentProps) {
   return (
     <Link href={links.product + `${data.id}/${stringToUrl(data.name)}`}>
-      <div className="flex items-center border border-gray-primary rounded w-full gap-2">
-        <div className="w-2/5 h-[98px] max-w-[98px] relative">
-          <Image
-            className="w-full h-full object-contain"
-            src={data.image}
-            alt={data.image}
-            fill
-          />
-        </div>
-        <div className="w-3/5 flex flex-col justify-between h-full pr-[18px] py-5">
-          <Rating
-            sx={{
-              "& .MuiSvgIcon-root": {
-                fontSize: "16px",
-              },
-            }}
-            name="read-only"
-            value={data.rating}
-            readOnly
-          />
-          <h4 className="line-clamp-1 text-[#374151] text-sm my-2">
-            {data.name}
-          </h4>
+      <WrapperAnimation hover={{}}>
+        <div className="flex items-center border border-gray-primary rounded w-full gap-2 hover:border-[#ee4d2d]">
+          <div className="w-2/5 h-[98px] max-w-[98px] relative">
+            <Image
+              className="w-full h-full object-contain"
+              src={data.image}
+              alt={data.image}
+              fill
+            />
+          </div>
+          <div className="w-3/5 flex flex-col justify-between h-full pr-[18px] py-5">
+            <Rating
+              sx={{
+                "& .MuiSvgIcon-root": {
+                  fontSize: "16px",
+                },
+              }}
+              name="read-only"
+              value={data.rating}
+              readOnly
+            />
+            <h4 className="line-clamp-1 text-[#374151] text-sm my-2">
+              {data.name}
+            </h4>
 
-          <div className="">
-            <span className="text-[#EF4444] text-lg font-medium">
-              {toCurrency(data.price)}
-            </span>
-            <del className="text-black-main text-sm ml-[10px] ">
-              {toCurrency(data.oldPrice)}
-            </del>
+            <div className="flex flex-col sm:flex-row">
+              <span className="text-[#EF4444] text-lg font-medium">
+                {toCurrency(data.price)}
+              </span>
+              <del className="text-black-main text-sm ml-[10px] max-w-full ">
+                {toCurrency(data.oldPrice)}
+              </del>
+            </div>
           </div>
         </div>
-      </div>
+      </WrapperAnimation>
     </Link>
   );
 }
