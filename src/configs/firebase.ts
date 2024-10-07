bca02938dd4de65b8203e5b9b788512a7c419481
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-07 20:17:57 +0700

    fix bug

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -3,23 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAgAGJmzZGjMpHAkj1xJtceyvtxyZIbZ94",
-//   authDomain: "chat-app-petfoster.firebaseapp.com",
-//   projectId: "chat-app-petfoster",
-//   storageBucket: "chat-app-petfoster.appspot.com",
-//   messagingSenderId: "651379250403",
-//   appId: "1:651379250403:web:9c95dac0505b3d8a8521d6",
-// };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
