40e7086ad2435f44b05650d4459e84171d323dac
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-08 15:01:25 +0700

    done cart slice for add cart and getcart

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,11 @@
-import { adorableReducer, userReducer } from "@/redux/slice";
+import { adorableReducer, cartReducer, userReducer } from "@/redux/slice";
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 export const store = configureStore({
   reducer: {
     userReducer,
     adorableReducer,
+    cartReducer,
   },
 });
 setupListeners(store.dispatch);
