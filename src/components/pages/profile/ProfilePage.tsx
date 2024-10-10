d3fcfc556af6ca2fd1647bad9548e448d45421d9
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-10 17:43:55 +0700

    done update profile

--- src/components/pages/profile/ProfilePage.tsx
+++ src/components/pages/profile/ProfilePage.tsx
@@ -2,31 +2,44 @@
 import classNames from "classnames";
 import React, {
   ChangeEvent,
-  FocusEventHandler,
+  FocusEvent,
   FormEvent,
+  useEffect,
   useState,
 } from "react";
 import style from "./styles/profile.module.css";
 import { Avatar } from "@mui/material";
 import { contants } from "@/utils/constant";
 import { ProfileType, RootState } from "@/configs/types";
-import { useAppSelector } from "@/hooks/reduxHooks";
+import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import DivTextfield from "@/components/common/inputs/DivTextField";
+import MainButton from "@/components/buttons/MainButton";
+import Confirm from "@/components/common/inputs/Confirm";
+import LoadingPrimary from "@/components/common/loadings/LoadingPrimary";
+import AvatarEdit from "@/components/common/inputs/AvatarEdit";
+import { pushNoty } from "@/redux/slice/appSlice";
+import { fetchUserByToken } from "@/redux/slice/userSlice";
+import { useRouter } from "next/navigation";
+import moment from "moment";
+import Validate from "@/utils/validate";
+import { updateCurrentUser } from "firebase/auth";
+import { updateUser } from "@/apis/user";
+import { toast } from "react-toastify";
 export interface IProfilePageProps {
   pages: [string];
 }
 
 const initdata = {
-  fullname: "",
+  fullName: "",
   email: "",
   phone: "",
   gender: "Male",
   birthday: "",
 };
 const initdataErrors = {
-  fullname: "",
+  fullName: "",
   email: "",
   phone: "",
   gender: "",
@@ -38,11 +51,135 @@ export default function ProfilePage({ pages }: IProfilePageProps) {
   const [openEditor, setOpenEditor] = useState(false);
   const [form, setForm] = useState<ProfileType>(initdata);
   const [errors, setErrors] = useState<ProfileType>(initdataErrors);
-  const handleOpenConfirm = (e: FormEvent<HTMLFormElement>) => {};
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {};
-  const handleBlur = (
-    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {};
+  const [showBtn, setShowBtn] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [openConfirm, setOpenConfirm] = useState({
+    open: false,
+    confirm: "cancel",
+  });
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const handleOpenConfirm = (e?: FormEvent<HTMLSpanElement>) => {
+    e?.preventDefault();
+    setOpenConfirm({ ...openConfirm, open: true });
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+    const dynamicKey = e.target.name as keyof ProfileType;
+    const { message } = Validate[dynamicKey](e.target.value);
+    setErrors({
+      ...errors,
+      [dynamicKey]: message,
+    });
+  };
+  const handleConfirm = async (v: {
+    open: boolean;
+    confirm: "cancel" | "ok";
+  }) => {
+    if (v.open || v.confirm === "cancel") return;
+
+    await handleSubmit();
+  };
+  const handleSubmit = async () => {
+    if (validate()) return;
+    try {
+      setLoading(true);
+      const response = await updateUser({
+        ...form,
+        avatar: avartar,
+      });
+      setLoading(false);
+      if (response.errors) {
+        setErrors({
+          ...errors,
+          ...(response.errors as unknown as ProfileType),
+        });
+
+        return;
+      }
+
+      toast.success("Update Successfuly");
+
+      dispatch(fetchUserByToken());
+
+      router.refresh();
+    } catch (error) {
+      console.log("errors in product page when update" + error);
+      setLoading(false);
+      dispatch(fetchUserByToken());
+      router.push("/");
+    }
+  };
+
+  //validate form before submit
+  const validate = () => {
+    let flag = false;
+    const validateErrors: ProfileType = { ...initdataErrors };
+    const keys: string[] = Object.keys(validateErrors);
+    keys.forEach((key) => {
+      const dynamic = key as keyof ProfileType;
+      const { message, error } = Validate[dynamic](form[dynamic]);
+      validateErrors[dynamic] = message;
+      flag = flag || error;
+    });
+    setErrors(validateErrors);
+    return flag;
+  };
+  useEffect(() => {
+    if (!user) {
+      dispatch(
+        pushNoty({
+          title: "Some thing went wrong, please re-login to use continue !",
+          open: true,
+          type: "error",
+        })
+      );
+      dispatch(fetchUserByToken());
+      router.push("/");
+      return;
+    }
+    setForm({
+      fullName: user?.fullName || "",
+      email: user?.email || "",
+      phone: user?.phone || "",
+      gender: user?.gender ? "Male" : "Female",
+      birthday: user?.birthday
+        ? moment(user?.birthday).format("YYYY-MM-DD")
+        : "",
+    });
+    setAvartar(user.avatar);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  useEffect(() => {
+    if (!user) return;
+
+    const initOjb = {
+      fullName: user?.fullName || "",
+      email: user?.email || "",
+      phone: user?.phone || "",
+      gender: user?.gender ? "Male" : "Female",
+      birthday: user?.birthday
+        ? moment(user?.birthday).format("yyyy-MM-D")
+        : "",
+    };
+
+    if (
+      JSON.stringify(initOjb) != JSON.stringify(form) ||
+      user.avatar != avartar
+    ) {
+      setShowBtn(true);
+    } else {
+      setShowBtn(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [form, avartar]);
   return (
     <>
       <form
@@ -81,13 +218,13 @@ export default function ProfilePage({ pages }: IProfilePageProps) {
           {/* text field */}
           <DivTextfield
             propsInput={{
-              name: "fullname",
-              value: form.fullname,
-              message: errors.fullname,
+              name: "fullName",
+              value: form.fullName,
+              message: errors.fullName,
               onChange: handleChange,
               onBlur: handleBlur,
             }}
-            label="Full name"
+            label="Full Name"
           />
 
           <div className="flex items-center gap-[22px] lg:gap-12 flex-col md:flex-row">
@@ -160,7 +297,28 @@ export default function ProfilePage({ pages }: IProfilePageProps) {
             </div>
           </div>
         </div>
+        <div className="flex items-center justify-center w-full mt-16">
+          {showBtn && <MainButton width={"208px"} title="update" />}
+        </div>
       </form>
+
+      <AvatarEdit
+        setOpen={setOpenEditor}
+        onAvartar={(avatar) => {
+          setAvartar(avatar || user?.avatar);
+        }}
+        open={openEditor}
+      />
+
+      {loading && <LoadingPrimary />}
+
+      <Confirm
+        title={"Notification"}
+        subtitle={"Are want to update your profile ?"}
+        open={openConfirm.open}
+        setOpen={setOpenConfirm}
+        onConfirm={handleConfirm}
+      />
     </>
   );
 }
