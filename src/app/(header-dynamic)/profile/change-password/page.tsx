21ffa27971bee400fe2be45410d738a845c900eb
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-10 21:30:36 +0700

    done change password and notification

---
+++ src/app/(header-dynamic)/profile/change-password/page.tsx
@@ -0,0 +1,10 @@
+import ChangePasswordPage from "@/components/pages/profile/ChangePasswordPage";
+import React from "react";
+
+export interface IChangePassAndAddressesProps {}
+
+export default function ChangePassAndAddresses(
+  props: IChangePassAndAddressesProps
+) {
+  return <ChangePasswordPage />;
+}
