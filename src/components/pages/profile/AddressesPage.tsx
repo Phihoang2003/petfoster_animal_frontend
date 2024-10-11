9028fb402ca85150aff3a36e54764a148fcc361c
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-11 22:54:05 +0700

    basic ui address

---
+++ src/components/pages/profile/AddressesPage.tsx
@@ -0,0 +1,66 @@
+"use client";
+import WrapperAnimation from "@/components/animations/WrapperAnimation";
+import AddressItem from "@/components/common/inputs/address/AddressItem";
+import LoadingSecondary from "@/components/common/loadings/LoadingSecondary";
+import BaseProfilePage from "@/components/pages/common/BaseProfilePage";
+import { IInfoAddress } from "@/configs/interface";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useQuery } from "@tanstack/react-query";
+import React, { createContext, useState } from "react";
+
+export const BaseProfilePageContext = createContext<{ refetch: () => any }>({
+  refetch: () => {},
+});
+export default function AddressesPage() {
+  const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const data: { data: IInfoAddress[] } = { data: [] };
+  const handleOpenConfirm = () => {};
+  const handleUpdate = () => {};
+  return (
+    <BaseProfilePageContext.Provider value={{ refetch: () => {} }}>
+      <BaseProfilePage
+        title="ADDRESS LIST"
+        action={
+          <WrapperAnimation hover={{}}>
+            {data && data.data.length < 4 && (
+              <button
+                onClick={() => setOpen(true)}
+                className="flex items-center justify-center py-2 px-4 bg-green-65a30d text-white font-medium text-1xl gap-1 hover:rounded transition-all ease-linear hover:bg-[#4b8614]"
+              >
+                <FontAwesomeIcon icon={faPlus} className="text-[18px]" />
+                <span>Add address</span>
+              </button>
+            )}
+          </WrapperAnimation>
+        }
+      >
+        <div className="">
+          {data && data?.data && (
+            <div key={"address-list"} className="w-full h-full">
+              {data.data.length > 0 ? (
+                data.data.map((address) => {
+                  return (
+                    <AddressItem
+                      handleEdit={handleUpdate}
+                      handleDelete={handleOpenConfirm}
+                      key={address.id}
+                      data={address}
+                    />
+                  );
+                })
+              ) : (
+                <div className="flex items-center justify-center h-[133px] border-b border-gray-primary">
+                  <span>{"You don't have a delivery address yet"}</span>
+                </div>
+              )}
+            </div>
+          )}
+
+          {isLoading && <LoadingSecondary />}
+        </div>
+      </BaseProfilePage>
+    </BaseProfilePageContext.Provider>
+  );
+}
