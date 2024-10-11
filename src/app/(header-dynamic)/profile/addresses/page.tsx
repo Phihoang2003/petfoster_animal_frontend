e898abbfbf701976bf7d6319b5164d1186ae0ddb
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-11 22:54:27 +0700

    show basic ui for address

---
+++ src/app/(header-dynamic)/profile/addresses/page.tsx
@@ -0,0 +1,8 @@
+import AddressesPage from "@/components/pages/profile/AddressesPage";
+import React from "react";
+
+export interface IAddressListProps {}
+
+export default function AddressList(props: IAddressListProps) {
+  return <AddressesPage />;
+}
