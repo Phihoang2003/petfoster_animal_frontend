24db05d199e887216e4cd0a6cc292fd55d866df3
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-12 18:46:16 +0700

    show base address dialog

--- src/components/pages/profile/AddressesPage.tsx
+++ src/components/pages/profile/AddressesPage.tsx
@@ -1,10 +1,12 @@
 "use client";
 import WrapperAnimation from "@/components/animations/WrapperAnimation";
+import AddressForm from "@/components/common/inputs/address/AddressForm";
 import AddressItem from "@/components/common/inputs/address/AddressItem";
+import BaseAddressDialog from "@/components/common/inputs/address/BaseAddressDialog";
 import LoadingSecondary from "@/components/common/loadings/LoadingSecondary";
 import BaseProfilePage from "@/components/pages/common/BaseProfilePage";
 import { IInfoAddress } from "@/configs/interface";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useQuery } from "@tanstack/react-query";
 import React, { createContext, useState } from "react";
@@ -16,8 +18,18 @@ export default function AddressesPage() {
   const [open, setOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const data: { data: IInfoAddress[] } = { data: [] };
+  const [updateData, setUpdateData] = useState<{
+    data: IInfoAddress | null;
+    updateMode: boolean;
+  }>({
+    data: null,
+    updateMode: false,
+  });
   const handleOpenConfirm = () => {};
   const handleUpdate = () => {};
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <BaseProfilePageContext.Provider value={{ refetch: () => {} }}>
       <BaseProfilePage
@@ -61,6 +73,58 @@ export default function AddressesPage() {
           {isLoading && <LoadingSecondary />}
         </div>
       </BaseProfilePage>
+
+      {/* dialog */}
+      <BaseAddressDialog
+        title={
+          <>
+            <div className="flex items-center ">
+              <h2 className="font-semibold uppercase">{"SHIPPING INFO"}</h2>
+            </div>
+            <WrapperAnimation
+              onClick={handleClose}
+              hover={{}}
+              className="flex items-center justify-center p-5 pr-0"
+            >
+              <FontAwesomeIcon className="cursor-pointer" icon={faXmark} />
+            </WrapperAnimation>
+          </>
+        }
+        open={open}
+        setOpen={setOpen}
+      >
+        {open && (
+          <AddressForm
+            showNotiAdopt={true}
+            initData={updateData.data || undefined}
+            updateMode={updateData.updateMode}
+            onBeforeAdd={() => {
+              requestIdleCallback(() => {
+                // refetch();
+                handleClose();
+              });
+            }}
+            onBeforeUpdate={() => {
+              requestIdleCallback(() => {
+                // refetch();
+                handleClose();
+              });
+            }}
+          />
+        )}
+
+        {/* <Comfirm
+                    title={'Notification'}
+                    subtitle={
+                        <>
+                            {'Are want to delete '} {deleteData && !deleteData.isDefault ? <b>{addressToString(deleteData?.address)}</b> : <b>{'default address'}</b>}
+                        </>
+                    }
+                    open={openComfirm.open}
+                    setOpen={setOpenComfirm}
+                    onComfirm={handleComfirm}
+                /> */}
+      </BaseAddressDialog>
     </BaseProfilePageContext.Provider>
   );
 }
