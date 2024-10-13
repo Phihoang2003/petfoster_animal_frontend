bfdef912673d040c8d05b4c59a9b3c3c4de51ad0
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-13 13:10:42 +0700

    done get api get default,get id,add address

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -2,10 +2,14 @@ import {
   ApiActionSearchHistories,
   ApiChangePassword,
   ApiCreateCartUser,
+  ApiGetAddresses,
+  ApiGetAddressesById,
   ApiGetCartUser,
   ApiGetCurUser,
   ApiGetCurUserWithUsername,
+  ApiGetDefaultAddress,
   ApiGetSearchHistories,
+  ApiHandleAddresses,
   ApiLogin,
   ApiRefreshVerifyCode,
   ApiRegister,
@@ -16,8 +20,13 @@ import {
 } from "@/configs/types";
 import axios from "../configs/axios";
 import { setTokenToCookie } from "@/utils/cookies";
-import { ICart, IFormChangePassword, ISearchItem } from "@/configs/interface";
-import { dataURLtoFile } from "@/utils/format";
+import {
+  ICart,
+  IFormChangePassword,
+  IInfoAddress,
+  ISearchItem,
+} from "@/configs/interface";
+import { dataURLtoFile, replaceValidDistrich } from "@/utils/format";
 import moment from "moment";
 export const register: ApiRegister = async (data) => {
   const res = await axios({
@@ -216,6 +225,58 @@ export const changePassword: ApiChangePassword = async (
     method: "POST",
     url: "user/profile/change-password",
     data,
+  });
+
+  if (!res) return null;
+
+  return res?.data;
+};
+
+export const getAddresses: ApiGetAddresses = async () => {
+  const res = await axios({
+    method: "GET",
+    url: "user/addresses",
+  });
+
+  if (!res) return null;
+
+  return res?.data;
+};
+
+export const getDefaultAddress: ApiGetDefaultAddress = async () => {
+  const res = await axios({
+    method: "GET",
+    url: "user/addresses/default",
+  });
+
+  if (!res) return null;
+
+  return res?.data;
+};
+
+export const getAddressesById: ApiGetAddressesById = async (id: number) => {
+  const res = await axios({
+    method: "GET",
+    url: "user/addresses/" + id,
+  });
+
+  if (!res) return null;
+
+  return res?.data;
+};
+
+export const addAddress: ApiHandleAddresses = async (data: IInfoAddress) => {
+  const res = await axios({
+    method: "POST",
+    url: "user/addresses",
+    data: {
+      ...data,
+      setDefault: data.isDefault,
+      address: {
+        ...data.address,
+        district: replaceValidDistrich(data.address.district),
+      },
+    },
   });
 
   if (!res) return null;
