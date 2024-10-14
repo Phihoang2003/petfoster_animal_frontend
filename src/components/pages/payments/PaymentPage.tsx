d81b8635c0378e395ad9b10a60cede1a2c6c34b7
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-14 15:39:53 +0700

    add addressinfo

--- src/components/pages/payments/PaymentPage.tsx
+++ src/components/pages/payments/PaymentPage.tsx
@@ -1,13 +1,21 @@
 "use client";
 import ContainerContent from "@/components/common/common-components/ContainerContent";
+import AddressInfoPayment from "@/components/common/inputs/address/AddressInfoPayment";
 import LineProgress from "@/components/pages/payments/LineProgress";
 import { links } from "@/data/links";
-import { Breadcrumbs } from "@mui/material";
+import { Breadcrumbs, Grid, Grid2, Stack } from "@mui/material";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function PaymentPage() {
   const [line, setLine] = useState(2);
+  const [isClient, setIsClient] = useState(false);
+  const handleOpenConfirm = () => {};
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <ContainerContent className="pt-12 select-none">
       <div role="presentation">
@@ -27,6 +35,27 @@ export default function PaymentPage() {
         </Breadcrumbs>
       </div>
       <LineProgress progressNun={line} />
+
+      {/* AddressInfo */}
+      <Grid2
+        container
+        spacing={"48px"}
+        mt={"34px"}
+        component={"form"}
+        onSubmit={handleOpenConfirm}
+      >
+        <Grid2 size={{ sm: 12, md: 12, lg: 6 }}>
+          {isClient && (
+            <Stack spacing={"40px"}>
+              <AddressInfoPayment />
+            </Stack>
+          )}
+        </Grid2>
+
+        <Grid2 size={{ sm: 12, md: 12, lg: 6 }}>
+          <div>Right side</div>
+        </Grid2>
+      </Grid2>
     </ContainerContent>
   );
 }
