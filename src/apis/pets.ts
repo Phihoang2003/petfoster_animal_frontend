6630216de265adc1e20e524a50842dc474acb2ab
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-20 15:24:25 +0700

    add petdetail api

--- src/apis/pets.ts
+++ src/apis/pets.ts
@@ -1,6 +1,10 @@
 import axios from "../configs/axios";
 import { IRequestFilterPet } from "@/configs/interface";
-import { ApiFilterPets, ApiPetAttributes } from "@/configs/types";
+import {
+  ApiFilterPets,
+  ApiPetAttributes,
+  ApiPetDetailPage,
+} from "@/configs/types";
 
 export const filterPets: ApiFilterPets = async (params: IRequestFilterPet) => {
   const res = await axios({
@@ -15,6 +19,17 @@ export const getPetAttibutes: ApiPetAttributes = async () => {
   const res = await axios({
     method: "GET",
     url: "/pets/attributes",
+  });
+
+  if (!res) return null;
+
+  return res?.data;
+};
+
+export const petDetail: ApiPetDetailPage = async (id: string) => {
+  const res = await axios({
+    method: "GET",
+    url: "/pets/" + id,
   });
 
   if (!res) return null;
