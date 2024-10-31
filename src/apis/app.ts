d18ece37941e2699ad35e4592600ea7b2ef579d7
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-10-31 21:10:49 +0700

    get type and  brands

--- src/apis/app.ts
+++ src/apis/app.ts
@@ -1,9 +1,37 @@
-import { ApiTakeActionType } from "@/configs/types";
+import {
+  ApiFilterPage,
+  ApiTakeActionType,
+  ApiTypesAndBrands,
+} from "@/configs/types";
 import axios from "../configs/axios";
+import { IDataRequestFilter } from "@/configs/interface";
 export const takeAction: ApiTakeActionType = async () => {
   const res = await axios({
     method: "GET",
     url: "take-action",
+  });
+
+  if (!res) return null;
+
+  return res?.data;
+};
+
+export const typesAndBrands: ApiTypesAndBrands = async () => {
+  const res = await axios({
+    method: "GET",
+    url: "product/types-brands",
+  });
+
+  if (!res) return null;
+
+  return res?.data;
+};
+
+export const filterPage: ApiFilterPage = async (data: IDataRequestFilter) => {
+  const res = await axios({
+    method: "GET",
+    url: "filter-product",
+    params: data,
   });
 
   if (!res) return null;
