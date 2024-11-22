18dcee520d0bbe1cb2ef6ecf2c8d183535320b8c
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-11-22 12:42:26 +0700

    add best seller

--- src/components/pages/take-action/LogicalTakeAction.tsx
+++ src/components/pages/take-action/LogicalTakeAction.tsx
@@ -1,14 +1,23 @@
 "use client";
+import { bestSellers } from "@/apis/app";
 import Products from "@/components/products-and-pets/Products";
 import { takeActionPageData } from "@/data/take-action";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 export default function LogicalTakeAction() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["product/logicalTakeAction"],
+    queryFn: () => bestSellers(0),
+  });
   return (
     <>
       <Products
         id="best-sellers"
-        data={takeActionPageData?.bestSellers?.data || []}
+        loading={isLoading}
+        data={
+          error ? takeActionPageData.bestSellers.data : data?.data?.data || []
+        }
         title="BEST SELLERS"
       />
     </>
