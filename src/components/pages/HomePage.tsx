e49e222b5700255b5fc98c56c03dbcf48220073c
Đặng Hoàng Phi <hoangphi1232003@gmail.com>
2024-11-22 12:41:56 +0700

    done render recent view

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,12 +1,23 @@
+"use client";
 import React from "react";
 import { homePageData } from "@/data/home-page";
 import ImpactOfYear from "@/components/impacts/ImpactOfYear";
 import AboutCom from "@/components/common/common-components/AboutCom";
 import Pets from "@/components/products-and-pets/Pets";
 import { links } from "@/data/links";
-import { dataTakeAction } from "@/data/adopt";
 import Feedback from "@/components/common/common-components/Feedback";
+import { useQuery } from "@tanstack/react-query";
+import { homepage } from "@/apis/app";
+import { notFound } from "next/navigation";
 export default function HomePage() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["homepage"],
+    queryFn: () => homepage(),
+  });
+
+  if (error || data?.errors) {
+    notFound();
+  }
   return (
     <>
       <ImpactOfYear data={(homePageData && homePageData.impactOfYear) || []} />
@@ -16,7 +27,7 @@ export default function HomePage() {
           buttonTitle: "See all",
           href: links.pets.adoptPage,
         }}
-        data={(dataTakeAction && dataTakeAction.pets) || []}
+        data={(data && data.data.pets) || []}
       />
       <Feedback />
     </>
